120432196e2b98ca0f64f89164c8f44675060618
jhy979 <jhy979@gmail.com>
2021-10-09 21:11:54 +0900

    날씨 화면 기본 레이아웃 작성

--- App.js
+++ App.js
@@ -5,17 +5,52 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello I made a RN App!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="auto"/>
+
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>  
+        </View>
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    flex:1, 
+    backgroundColor:"#ebd8ad"
   },
-});
+  city :{
+    flex:1,
+    backgroundColor:"#274722",
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  cityName:{
+    color:"black",
+    fontSize:68,
+    fontWeight: "500",
+  },
+  weather:{
+    flex:3,
+  },
+  day: {
+    flex:1,
+    alignItems:'center',
+  },
+  temp:{
+    fontSize:150,
+    marginTop:70,
+  },
+  description:{
+    marginTop: -30,
+    fontSize: 40,
+  }
+})
